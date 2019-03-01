a2cca438d9eea872d59a203c4a3f5ba141d4b12f
CharlieLu97 <luh15@uci.edu>
2019-03-01 00:12:49 -0800

    pull

--- mobile/app/src/main/java/com/example/diabetedefender/MainActivity.java
+++ mobile/app/src/main/java/com/example/diabetedefender/MainActivity.java
@@ -1,30 +1,23 @@
 package com.example.diabetedefender;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements
         BottomNavigationView.OnNavigationItemSelectedListener {
 
 
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        BottomNavigationView navigation =  findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(this);
-
-        loadFragment(new HomeFragment());
-    }
 
     private boolean loadFragment(Fragment fragment){
         if(fragment !=null){
@@ -37,6 +30,21 @@ public class MainActivity extends AppCompatActivity implements
         return false;
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation =  findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 
 
     @Override
@@ -57,9 +65,42 @@ public class MainActivity extends AppCompatActivity implements
                 break;
 
         }
-
-
         return loadFragment(fragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.help:
+
+                Intent help = new Intent(this,HelpActivity.class);
+                startActivity(help);
+
+                break;
+            case R.id.settings:
+
+
+                Intent settings = new Intent(this,SettingsActivity.class);
+                startActivity(settings);
+
+                break;
+            case R.id.update:
+                Toast.makeText(getApplicationContext(),"Latest Version Installed!",Toast.LENGTH_SHORT).show();
+                Intent update = new Intent(this,UpdateActivity.class);
+                startActivity(update);
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
